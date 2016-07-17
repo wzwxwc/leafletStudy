e8a1541f3de1578bdd6fbd7c6394e1616287091b
wzwxwc <zc_1987>
2016-07-17 21:23:39 +0800

    no coomments from zcRescuer

--- codes1/02批量定位/main.js
+++ codes1/02批量定位/main.js
@@ -2,6 +2,7 @@
  * Created by zcG460 on 2016/7/15.
  */
 var mymap = null;
+var elementContainer = [];
 window.onload = function () {
     mymap = L.map('mapid').setView([51.505, -0.09], 13);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
@@ -11,6 +12,7 @@ window.onload = function () {
         'Imagery © <a href="http://mapbox.com">Mapbox</a>',
         id: 'mapbox.streets'
     }).addTo(mymap);
+
     $("#MULTIPOINT").click(function () {
         zoomToMULTIPOINT();
     });
@@ -19,14 +21,26 @@ window.onload = function () {
     });
 
     $("#btn3").click(function () {
-        var arrLatLng = [
-            [51.49869827721546, -0.19002914428710938],
-            [51.51301590715673, -0.1560401916503906],
-            [51.50810140697543, -0.12805938720703125]
-        ];
-        L.polyline(arrLatLng, {
+        // var arrLatLng = [
+        //     [51.49869827721546, -0.19002914428710938],
+        //     [51.51301590715673, -0.1560401916503906],
+        //     [51.50810140697543, -0.12805938720703125]
+        // ];
+        var arrLatLng = [];
+        arrLatLng.push([51.49869827721546, -0.19002914428710938]);
+        arrLatLng.push([51.51301590715673, -0.1560401916503906]);
+        arrLatLng.push([51.50810140697543, -0.12805938720703125]);
+        elementContainer.push(L.polyline(arrLatLng, {
             color: "red"
-        }).addTo(mymap);
+        }).addTo(mymap));
+        mymap.fitBounds(arrLatLng);
+    });
+
+    $("#btnClear").click(function () {
+        for (var i = 0; i < elementContainer.length; i++) {
+            mymap.removeLayer(elementContainer[i]);
+        }
+        elementContainer = [];
     });
 };
 
@@ -44,15 +58,23 @@ function zoomToMULTIPOINT(show) {
 function zoomToLINESTRING(show) {
     var data = "LINESTRING(-0.19002914428710938 51.49869827721546,-0.1560401916503906 51.51301590715673,-0.12805938720703125 51.50810140697543)";
     var latLngBounds = fnParseWkt(data);
+    elementContainer.push(L.polyline(latLngBounds, {
+        color: "red"
+    }).addTo(mymap));
     mymap.fitBounds(latLngBounds);
 }
 
 /**
  * 缩放到面集合
  */
-function zoomToPolygon() {
+function zoomToPOLYGON() {
+    var data = "POLYGON((-0.14574050903320312 51.51376371940495,-0.12531280517578122 51.50649873794456,-0.12342453002929686 51.49624032118747,-0.160675048828125 51.498270816123465))";
+    var dataParsed = fnParseWkt(data);
+    elementContainer.push(L.polygon(dataParsed));
+
 
 }
+
 /**
  * 显示所有的面
  */
@@ -66,9 +88,11 @@ function fnParseWkt(strWkt) {
         var strType = strWkt.substr(0, strWkt.indexOf('('));
         switch (strType) {
             case 'MULTIPOINT':
-                return wktUtil.getPointArrayFromMULTIPOINT(strWkt);
+                return wktUtil.getLatLngArrFromMULTIPOINT(strWkt);
             case "LINESTRING":
-                return wktUtil.getPointArrayFromLINESTRING(strWkt);
+                return wktUtil.getLatLngArrFromLINESTRING(strWkt);
+            case "POLYGON":
+                return wktUtil.getLatLngArrFromPOLYGON(strWkt);
             default:
                 break;
         }
--- codes1/02批量定位/wktUtil.js
+++ codes1/02批量定位/wktUtil.js
@@ -6,7 +6,7 @@
 
 //对WKT字符串进行相应的解析
 var wktUtil = {};
-wktUtil.getPointArrayFromMULTIPOINT = function (strWkt) {
+wktUtil.getLatLngArrFromMULTIPOINT = function (strWkt) {
     //输入strWkt： MULTIPOINT(3.5 5.6, 4.8 10.5)
     //输出arrPoints: [[3.5,5.6],[4.8,10.5]]
     var arrPoints = new Array();
@@ -21,7 +21,7 @@ wktUtil.getPointArrayFromMULTIPOINT = function (strWkt) {
     return arrPoints;
 };
 
-wktUtil.getPointArrayFromLINESTRING = function (strWkt) {
+wktUtil.getLatLngArrFromLINESTRING = function (strWkt) {
     //输入strWkt： LINESTRING(3 4,10 50,20 25)
     //输出arrPoints: [[3,4],[10,50],[20,25]]
     var arrPoints = [];
@@ -29,12 +29,40 @@ wktUtil.getPointArrayFromLINESTRING = function (strWkt) {
     var arrXYs = strPoints.split(',');
     for (var i = 0; i < arrXYs.length; i++) {
         var point = arrXYs[i].split(' ');
-        var pointLeaflet = [point[1], point[0]];
+        //注意：下述2个参数需要从字符串转换为数字
+        var pointLeaflet = [parseFloat(point[1]), parseFloat(point[0])];
         arrPoints.push(pointLeaflet);
     }
-    L.polyline(arrPoints, {
-        color: "red"
-    }).addTo(mymap);
+    return arrPoints;
+};
+
+wktUtil.getLatLngArrFromPOLYGON = function (strWkt) {
+    //输入strWkt： POLYGON ((1 1,5 1,5 5,1 5,1 1),(2 2,2 3,3 3,3 2,2 2))
+    /*输出arrPoints:
+     [
+     [1,1],[5,1],[5,5],[1,5],[1,1]
+     ],
+     [
+     [2,2],[2,3],[3,3],[3,2],[2,2]
+     ]*/
+    //输入：strWkt： POLYGON ((1 1,5 1,5 5,1 5,1 1))
+    /*
+     输出：
+     [1,1],[5,1],[5,5],[1,5],[1,1]
+     * */
+    var arrPoints = [];
+    //判断是否包含内边？
+    if (strWkt.indexOf('((') > -1) {
+
+    }
+    var strPoints = strWkt.substring(strWkt.indexOf('(') + 1, strWkt.indexOf(')'));
+    var arrXYs = strPoints.split(',');
+    for (var i = 0; i < arrXYs.length; i++) {
+        var point = arrXYs[i].split(' ');
+        //注意：下述2个参数需要从字符串转换为数字
+        var pointLeaflet = [parseFloat(point[1]), parseFloat(point[0])];
+        arrPoints.push(pointLeaflet);
+    }
     return arrPoints;
 };
 
