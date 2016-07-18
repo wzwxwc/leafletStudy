785bf3037b6f7c8c0614d1bd7f97d19428c0fcdc
wzwxwc <475977851@qq.com>
2016-07-18 15:12:14 +0800

    推进批量定位 from zcG460

---
+++ codes1/02批量定位/dataValueUtil.js
@@ -0,0 +1,56 @@
+/**
+ * Created by zcG460 on 2016/7/18.
+ * 处理业务数据
+ */
+dataValueUtil = {};
+dataValueUtil.deal = function (value) {
+    /*
+     中英文对照
+     名称                            name
+     ※※记录ID                      id
+     ※※点、线、面的wkt格式数据     wkt
+     ※※显示信息                    info
+     @@
+     名称                            name
+     ※※记录ID                      id
+     ※※经度值                      lon
+     ※※纬度                        lat
+     ※※显示信息                    info
+     @@……”*/
+    var recordList = {};
+    var recordStrArr = value.split('@@');
+    for (var i = 0; i < recordStrArr.length; i++) {
+        var recordStr = recordStrArr[i];
+        var fieldStrArr = recordStr.split('※※');
+        var record = {};
+        record.name = fieldStrArr[0];
+        record.id = fieldStrArr[1];
+
+        if (isNaN(parseFloat(fieldStrArr[2]))) {
+            //按照wkt的方式来解析
+            record.wkt = fieldStrArr[2];
+            var wkt=record.wkt;
+            record.info = fieldStrArr[3];
+            if (wkt.indexOf('POINT') >= 0) {
+                record.geotype = "1";
+            } else if (wkt.indexOf('LINESTRING')>=0) {
+                record.geotype = "2";
+            } else if (wkt.indexOf('POLYGON')>=0) {
+                record.geotype = "3";
+            } else {
+                console.lon("id 为" + fieldStrArr[1] + "的记录不存在有效的位置信息。");
+                continue;
+            }
+
+        } else {
+            //按照经纬度的方式来解析
+            record.lon = fieldStrArr[2];
+            record.lat = fieldStrArr[3];
+            record.info = fieldStrArr[4];
+            record.wkt = "POINT(" + record.lon + " " + record.lat + ")";
+            record.geotype = "1";
+        }
+        recordList[record.id] = record;
+    }
+    return recordList;
+};
--- codes1/02批量定位/wktUtil.js
+++ codes1/02批量定位/wktUtil.js
@@ -6,16 +6,27 @@
 
 //对WKT字符串进行相应的解析
 var wktUtil = {};
+wktUtil.getLatLngFromPOINT = function (strWkt) {
+    //输入strWkt  POINT(6 10)
+    //输出point   [10,6]
+    var pointLeaflet = [];
+    var strPoint = strWkt.substring(strWkt.indexOf('(') + 1, strWkt.indexOf(')'));
+    var arrXY = strPoint.split(' ');
+    pointLeaflet.push(parseFloat(arrXY[1]));
+    pointLeaflet.push(parseFloat(arrXY[0]));
+    return pointLeaflet;
+};
+
 wktUtil.getLatLngArrFromMULTIPOINT = function (strWkt) {
     //输入strWkt： MULTIPOINT(3.5 5.6, 4.8 10.5)
     //输出arrPoints: [[3.5,5.6],[4.8,10.5]]
     var arrPoints = new Array();
     var strPoints = strWkt.substring(strWkt.indexOf('(') + 1, strWkt.indexOf(')'));
+
     var arrXYs = strPoints.split(',');
     for (var i = 0; i < arrXYs.length; i++) {
         var point = arrXYs[i].split(' ');
         var pointLeaflet = [point[1], point[0]];
-        L.marker(pointLeaflet).addTo(mymap);
         arrPoints.push(pointLeaflet);
     }
     return arrPoints;
@@ -25,44 +36,87 @@ wktUtil.getLatLngArrFromLINESTRING = function (strWkt) {
     //输入strWkt： LINESTRING(3 4,10 50,20 25)
     //输出arrPoints: [[3,4],[10,50],[20,25]]
     var arrPoints = [];
-    var strPoints = strWkt.substring(strWkt.indexOf('(') + 1, strWkt.indexOf(')'));
-    var arrXYs = strPoints.split(',');
-    for (var i = 0; i < arrXYs.length; i++) {
-        var point = arrXYs[i].split(' ');
-        //注意：下述2个参数需要从字符串转换为数字
-        var pointLeaflet = [parseFloat(point[1]), parseFloat(point[0])];
-        arrPoints.push(pointLeaflet);
-    }
-    return arrPoints;
+    var strPoints = strWkt.substring(strWkt.indexOf('('), strWkt.indexOf(')') + 1);
+    return this.fnDealWithCell(strPoints);
 };
 
 wktUtil.getLatLngArrFromPOLYGON = function (strWkt) {
-    //输入strWkt： POLYGON ((1 1,5 1,5 5,1 5,1 1),(2 2,2 3,3 3,3 2,2 2))
-    /*输出arrPoints:
-     [
+    /*
+     第1种可能：
+     输入strWkt： POLYGON ((1 1,5 1,5 5,1 5,1 1),(2 2,2 3,3 3,3 2,2 2))
+     输出arrPoints:
+     [[
      [1,1],[5,1],[5,5],[1,5],[1,1]
      ],
      [
      [2,2],[2,3],[3,3],[3,2],[2,2]
-     ]*/
-    //输入：strWkt： POLYGON ((1 1,5 1,5 5,1 5,1 1))
+     ]]
+     */
     /*
-     输出：
-     [1,1],[5,1],[5,5],[1,5],[1,1]
+     第2种可能：
+     输入：strWkt： POLYGON ((1 1,5 1,5 5,1 5,1 1))
+     输出：[[1,1],[5,1],[5,5],[1,5],[1,1]]
      * */
     var arrPoints = [];
     //判断是否包含内边？
-    if (strWkt.indexOf('((') > -1) {
-
+    if (strWkt.indexOf('),(') > -1) {
+        //存在内边
+        /*
+         第1种可能：
+         输入strWkt： POLYGON ((1 1,5 1,5 5,1 5,1 1),(2 2,2 3,3 3,3 2,2 2))
+         输出arrPoints:
+         [[
+         [1,1],[5,1],[5,5],[1,5],[1,1]
+         ],
+         [
+         [2,2],[2,3],[3,3],[3,2],[2,2]
+         ]]
+         */
+        var cell1 = strWkt.substring(strWkt.indexOf('((') + 1, strWkt.indexOf('),(') + 1);
+        var cell2 = strWkt.substring(strWkt.indexOf('),(') + 2, strWkt.indexOf('))') + 1);
+        arrPoints.push(this.fnDealWithCell(cell1));
+        arrPoints.push(this.fnDealWithCell(cell2));
+        return arrPoints;
+    } else {
+        //不存在内边
+        /*第2种可能：
+         输入：strWkt： POLYGON ((1 1,5 1,5 5,1 5,1 1))
+         输出：[[1,1],[5,1],[5,5],[1,5],[1,1]]* */
+        var cell = strWkt.substring(strWkt.indexOf('((') + 1, strWkt.indexOf('))') + 1);
+        //下述push返回的值是1
+        arrPoints.push(this.fnDealWithCell(cell));
+        return arrPoints;
     }
-    var strPoints = strWkt.substring(strWkt.indexOf('(') + 1, strWkt.indexOf(')'));
+};
+
+
+/**
+ * 处理最小的“字符串单元”
+ //输入(3 4,10 50,20 25)
+ //输出[[3,4],[10,50],[20,25]]
+ */
+wktUtil.fnDealWithCell = function (cell) {
+    var results = [];
+    var strPoints = cell.substring(cell.indexOf('(') + 1, cell.indexOf(')'));
     var arrXYs = strPoints.split(',');
     for (var i = 0; i < arrXYs.length; i++) {
         var point = arrXYs[i].split(' ');
         //注意：下述2个参数需要从字符串转换为数字
         var pointLeaflet = [parseFloat(point[1]), parseFloat(point[0])];
-        arrPoints.push(pointLeaflet);
+        results.push(pointLeaflet);
     }
-    return arrPoints;
+    return results;
 };
 
+
+/*
+ 改进：
+ (1 1,5 1,5 5,1 5,1 1)
+ 对上述这个，转换为LatLngArr
+ 这个功能可以单独提取出来
+
+
+ 新方案，仅仅从字符串替换的角度（用replace方法）
+ 生成一个合适的字符串字面量
+ 然后直接赋值给一个变量（eval的方式）
+ * */
