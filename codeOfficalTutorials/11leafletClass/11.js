3a0a46bf07362ed66c57c01eef16b88ed3ce4c2e
wzwxwc <475977851@qq.com>
2017-11-09 18:13:50 +0800

    no comments from zcRescuer

--- codeOfficalTutorials/11leafletClass/11.js
+++ codeOfficalTutorials/11leafletClass/11.js
@@ -3,34 +3,40 @@ window.onload = function () {
     map = zcMap.fnInitTdt("mapid");
 };
 
+
 //————————————————————————
 var MyDemoClass = L.Class.extend({
     // A property with initial value = 42
-    myDemoProperty: 42,
+    zc_p1: 42,
+    zc_p2: [1, 2],
     // A method
-    myDemoMethod: function () {
-        return this.myDemoProperty;
+    zc_m1: function () {
+        return this.zc_p1;
     }
 });
-var myDemoInstance = new MyDemoClass();
+var a = new MyDemoClass();
+var b = new MyDemoClass();
 // This will output "42" to the development console
-console.log(myDemoInstance.myDemoMethod());
+console.log(a.zc_m1());
 
 //————————————————————————
-MyDemoClass.include({
-    // Adding a new property to the class
-    _myPrivateProperty: 78,
-    // Redefining a method
-    myDemoMethod: function () {
-        return this._myPrivateProperty;
-    }
-});
-var mySecondDemoInstance = new MyDemoClass();
-// This will output "78"
-//下述这个怎么删不掉属性？
-//回答，因为这个属性不是实例属性，而是一个prototype的属性
-delete mySecondDemoInstance.myDemoMethod;
-console.log(mySecondDemoInstance.myDemoMethod());
-// However, properties and methods from before still exist
-// This will output "42"
-console.log(mySecondDemoInstance.myDemoProperty);
+
+function fnInclude() {
+    MyDemoClass.include({
+        // Adding a new property to the class
+        _myPrivateProperty: 78,
+        // Redefining a method
+        myDemoMethod: function () {
+            return this._myPrivateProperty;
+        }
+    });
+    var mySecondDemoInstance = new MyDemoClass();
+    // This will output "78"
+    // 下述这个怎么删不掉属性？
+    // 回答，因为这个属性不是实例属性，而是一个prototype的属性
+    delete mySecondDemoInstance.myDemoMethod;
+    console.log(mySecondDemoInstance.myDemoMethod());
+    // However, properties and methods from before still exist
+    // This will output "42"
+    console.log(mySecondDemoInstance.myDemoProperty);
+}
