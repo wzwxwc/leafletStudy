3ba3bdf91788c536a8ddc18172214c46bc2090b2
wzwxwc <475977851@qq.com>
2017-12-26 19:53:01 +0800

    no comments from zcRescuer

--- codeOfficalTutorials/05leafletClass/MyMarker.js
+++ codeOfficalTutorials/05leafletClass/MyMarker.js
@@ -4,16 +4,23 @@
  * 功能：扩展现有的Marker
  */
 L.MyMarker = L.Marker.extend({
-    initialize: function (latlng, options) {
-        L.Marker.prototype.initialize.call(this, latlng, options);
+    //构造函数initialize如果省略，会自动调用父类的构造函数
+    // initialize: function (latlng, options) {
+    //     L.Marker.prototype.initialize.call(this, latlng, options);
+    //     this.bindPopup("你好", {
+    //         autoPan: true,
+    //         keepInView: true,
+    //         autoClose: false
+    //     }).openPopup();
+    // },
+    onAdd: function (map) {
+        L.Marker.prototype.onAdd.call(this, map);
+        //下述的this是指什么？MyMarker类的实例对象吗？
         this.bindPopup("你好", {
             autoPan: true,
             keepInView: true,
             autoClose: false
         }).openPopup();
-    },
-    onAdd: function (map) {
-        L.Marker.prototype.onAdd.call(this, map);
         this.openPopup();
     }
 });
---
+++ codeOfficalTutorials/06tileLayerPhoto/TileLayer.Photo.js
@@ -0,0 +1,37 @@
+/**
+ * 时间：2017/12/26
+ * 作者：张超
+ * 功能：显示照片的图层扩展类
+ */
+if (!L.TileLayer) {
+    L.TileLayer = {};
+}
+L.TileLayer.Photo = L.TileLayer.extend({
+    getTileUrlOld: function (coords) {
+        var data = {
+            r: Browser.retina ? '@2x' : '',
+            s: this._getSubdomain(coords),
+            x: coords.x,
+            y: coords.y,
+            z: this._getZoomForUrl()
+        };
+        if (this._map && !this._map.options.crs.infinite) {
+            var invertedY = this._globalTileRange.max.y - coords.y;
+            if (this.options.tms) {
+                data['y'] = invertedY;
+            }
+            data['-y'] = invertedY;
+        }
+
+        return Util.template(this._url, Util.extend(data, this.options));
+    },
+    getTileUrl: function (coords) {
+        var i = Math.ceil(Math.random() * 8);
+        return "./img/b" + i + ".jpg";
+    }
+});
+
+L.tileLayer.photo = function () {
+    return new L.TileLayer.Photo();
+};
+
