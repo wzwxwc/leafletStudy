3d5efce46ed6a7ac1e94ca53ee7fbaafd07cae36
wzwxwc <475977851@qq.com>
2018-01-22 19:57:42 +0800

    标绘组件的修改

--- 00commonLibs/mapUtil.js
+++ 00commonLibs/mapUtil.js
@@ -21,7 +21,7 @@ mapUtil.fnInitBaseMap = function (argStrMapDivId) {
 };
 
 mapUtil.fnInitTdt = function (argStrMapDivId) {
-    var mymap = L.map(argStrMapDivId);
+    var mymap = L.map(argStrMapDivId).setView([39.889718875996685, -243.5579681396484], 10);
     L.tileLayer("http://t{s}.tianditu.cn/img_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=img&tileMatrixSet=w&TileMatrix={z}&TileRow={y}&TileCol={x}&style=default&format=tiles", {
         subdomains: ["0", "1", "2", "3", "4", "5", "6", "7"]
     }).addTo(mymap);
